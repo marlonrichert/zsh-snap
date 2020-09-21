74fca19d5e1f1cfb62157b527ec423375833ec75
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-09-21 23:24:54 +0300

    Split functions into own files

---
+++ functions/.znap.ls
@@ -0,0 +1,9 @@
+#!/bin/zsh
+# ls plugins dir or repo
+emulate -L zsh -o extendedglob -o NO_shortloops -o warncreateglobal
+
+local -a files=()
+local repo=$(.znap.path $1)
+[[ -d $repo ]] && files+=( $repo )
+[[ -f $cache ]] && files+=( $cache )
+(( $#files > 0 )) && eval "ls -l $files"
--- znap.zsh
+++ znap.zsh
@@ -1,3 +1,9 @@
 #!/bin/zsh
-typeset -gU FPATH fpath=( ${${(%):-%x}:A:h} $fpath )
-autoload -Uz znap
+() {
+  emulate -L zsh -o extendedglob -o NO_shortloops -o warncreateglobal
+
+  local basedir=${${(%):-%x}:A:h}
+  local funcdir=$basedir/functions
+  typeset -gU FPATH fpath=( $funcdir $basedir $fpath )
+  autoload -Uz znap $funcdir/.znap.*
+}
