db4cc7a5f6d7b86b7751c1881b7f773b7f100fd9
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-09-21 23:25:41 +0300

    Add asynchronous auto-compilation

--- znap.zsh
+++ znap.zsh
@@ -6,4 +6,22 @@
   local funcdir=$basedir/functions
   typeset -gU FPATH fpath=( $funcdir $basedir $fpath )
   autoload -Uz znap $funcdir/.znap.*
+
+  if zstyle -t :znap: auto-compile; then
+    source() {
+      znap compile "$1:A" $funcstack[@]
+      builtin source "$@"
+    }
+    .() {
+      znap compile "$1:A" $funcstack[@]
+      builtin . "$@"
+    }
+    :znap:compile() {
+      add-zle-hook-widget -d line-init :znap:compile
+      znap compile
+    }
+    autoload -Uz add-zle-hook-widget
+    add-zle-hook-widget line-init :znap:compile
+    znap compile $funcstack[@]
+  fi
 }
