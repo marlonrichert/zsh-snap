4bde7284194ac6d98cbff50424b61612dda59671
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-11-24 15:59:57 +0200

    Don't autoload binaries

--- znap.zsh
+++ znap.zsh
@@ -1,14 +1,13 @@
 #!/bin/zsh
 () {
   emulate -L zsh
-
   typeset -gHa _znap_opts=( localoptions extendedglob globstarshort rcquotes )
   setopt $_znap_opts
 
   local basedir=${${(%):-%x}:A:h}
   local funcdir=$basedir/functions
   typeset -gU FPATH fpath=( $funcdir $basedir $fpath )
-  autoload -Uz znap $funcdir/.znap.*
+  autoload -Uz znap $funcdir/.znap.*~*.zwc
 
   if zstyle -T :znap: auto-compile; then
     zmodload zsh/parameter
