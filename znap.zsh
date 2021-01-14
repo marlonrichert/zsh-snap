a4ee1182be34da16850a624a20458f43d1e2b697
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-01-14 15:38:36 +0200

    Move init code to separate files

--- znap.zsh
+++ znap.zsh
@@ -7,104 +7,11 @@
   local basedir=${${(%):-%x}:A:h}
   local funcdir=$basedir/functions
   typeset -gU FPATH fpath=( $funcdir $basedir $fpath )
-  autoload -Uz znap $funcdir/.znap.*~*.zwc
+  autoload -Uz znap $funcdir/*znap*~*.zwc
 
   local pluginsdir; zstyle -s :znap: plugins-dir pluginsdir ||
     pluginsdir=$basedir:h
   hash -d znap=$pluginsdir
 
-  :znap:dirname() {
-    local repo
-    case $1 in
-      n)
-        repo=~znap/$2
-        if [[ -d $repo ]]; then
-          reply=( $repo )
-          return 0
-        fi
-        ;;
-      d)
-        for repo in ~znap/*(-/); do
-          if [[ $2 == $repo(|/*) ]]; then
-            reply=( $repo:t $#repo )
-            return 0
-          fi
-        done
-        ;;
-      c)
-        local expl tag='named-directories' group='named directory'
-        _tags $tag
-        _tags &&
-          _requested $tag expl $group &&
-            compadd "$expl[@]" -I ']' -Q - ~znap/*(on-/:t)
-        return
-        ;;
-    esac
-    return 72
-  }
-  export -U zsh_directory_name_functions=(
-    :znap:dirname
-    $zsh_directory_name_functions[@]
-  )
-
-
-  export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-  if [[ ! -d $XDG_CACHE_HOME ]]; then
-    zmodload -F zsh/files b:zf_mkdir
-    zf_mkdir -p $XDG_CACHE_HOME
-  fi
-
-  zstyle -s :completion: cache-path cache_path ||
-    zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zcompcache"
-
-  typeset -gH _comp_dumpfile=${_comp_dumpfile:-$XDG_CACHE_HOME/zcompdump}
-  if [[ -f $_comp_dumpfile && ${${:-${ZDOTDIR:-$HOME}/.zshrc}:A} -nt $_comp_dumpfile ]]; then
-    zmodload -F zsh/files b:zf_rm
-    zf_rm -f $_comp_dumpfile
-  fi
-
-  if zstyle -T :znap: auto-compile; then
-    zmodload -Fa zsh/parameter p:funcstack
-
-    source . () {
-      builtin $funcstack[1] "$@"; local -i ret=$?
-      .znap.compile "$1:A" ${(M@)funcstack[@]:#*/*}
-      return ret
-    }
-
-    local -a exclude=()
-    zstyle -a :znap: auto-compile-ignore exclude
-    local -a include=(
-      ${(M@)funcstack[@]:#*/*}
-      $_comp_dumpfile
-      $^fpath/*~*.zwc(-^/)
-      ${ZDOTDIR:-$HOME}/.z(log(in|out)|profile|sh(env|rc))(-^/)
-    )
-    .znap.compile ${include:#(${(~j:|:)~exclude})}
-  fi
-
-  .znap.function compdef '
-    autoload -Uz compinit
-    compinit -C -d $_comp_dumpfile
-    compinit() { : }
-  '
-  .znap.function _bash_complete compgen complete '
-    autoload -Uz bashcompinit
-    bashcompinit
-    bashcompinit() { : }
-  '
-
-  :znap:compinit() {
-    add-zsh-hook -d precmd ${(%):-%N}
-    unfunction ${(%):-%N}
-
-    if [[ ! -v _comp_setup || ! -f $_comp_dumpfile ]]; then
-      autoload -Uz compinit
-      compinit -C -d $_comp_dumpfile
-    fi
-
-    compinit() { : }
-  }
-  autoload -Uz add-zsh-hook
-  add-zsh-hook precmd :znap:compinit
+  :znap:init "$@"
 }
