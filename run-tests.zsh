077af09b9d8a46849d6e01c4dff1fc37e37917a7
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-04-24 18:14:38 +0300

    Add test runner

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,4 +1,5 @@
-#!/bin/zsh
-FPATH=$FPATH:${0:h}/functions zsh ~[clitest]/clitest \
+#!/bin/zsh -f
+print $OSTYPE $ZSH_PATCHLEVEL
+FPATH=$FPATH:${0:h}/functions zsh =clitest \
     --list-run --progress dot --prompt '%' --pre-flight ". ${0:h}/.znap.opts.zsh" \
     -- ${0:h}/.clitest/*.md
