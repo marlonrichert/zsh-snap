881342ca7e3dca87ca1f241313260e8ebf7fc97a
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-06-23 23:44:21 +0300

    Add more environment output to test runner

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh -f
-print $OSTYPE $SHELL $ZSH_VERSION $ZSH_PATCHLEVEL
+print $VENDOR $OSTYPE =zsh $ZSH_VERSION $ZSH_PATCHLEVEL
 FPATH=$FPATH:${0:h}/functions zsh -f -- =clitest \
     --list-run --progress dot --prompt '%' --pre-flight ". ${0:h}/.znap.opts.zsh" \
     -- ${0:h}/.clitest/*.md
